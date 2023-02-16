a60743b45bdf7f63427cc4525016bd05b7d6738a
jploeg <jploeg3450@student.landstede.nl>
2023-02-16 15:08:27 +0100

    added fetching for movies

---
+++ app/movies.js
@@ -0,0 +1,26 @@
+async function getMovies() {
+    const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=5b04841f2d7c4499c4a89e1dd286e67b')
+    return await data.json()
+}
+
+export default async function Movies() {
+
+    const res = await getMovies()
+    console.log(res)
+
+    return (
+        <div>
+            <h1 className="text-4xl my-5 mx-10">Movies</h1>
+            <ul className="w-1/3 mx-10">
+            {res.results.map(movie => (
+                <div key={movie.id} className="py-5">
+                    <li className="font-bold">{movie.title}</li>
+                    <li>{movie.overview}</li>
+                    <li>{movie.release_date}</li>
+                </div>
+                ))}
+            </ul>
+    
+        </div>
+    )
+}
